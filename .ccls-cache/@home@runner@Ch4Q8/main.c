04f3a7bf200e414e45e337624143cd5638a782a3
Iain Taylor <iain.taylor.94@gmail.com>
2023-06-26 13:23:55 +0000

    completed program

--- .ccls-cache/@home@runner@Ch4Q8/main.c
+++ .ccls-cache/@home@runner@Ch4Q8/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+
+  // First set of values
+  int i = 365, j = 7;
+  int nextMultiple = i + j - i % j;
+  printf ("The first multiple is: %i\n", nextMultiple);
+
+    // Second set of values
+  i = 12258, j = 23;
+  nextMultiple = i + j - i % j;
+  printf ("The second multiple is: %i\n", nextMultiple);
+
+    // Third set of values
+  i = 996, j = 4;
+  nextMultiple = i + j - i % j;
+  printf ("The third multiple is: %i\n", nextMultiple);
+
   return 0;
 }
---
+++ readme.md
@@ -0,0 +1,9 @@
+# programming in C
+## Chapter 4
+### Question 6
+
+This problem is from chapter 4 of the textbook, it is to round off an intiger to the next largest even multiple of another intiger.[^1]
+
+You may wish to run the program and compare its output.
+
+[^1]: Programming in C (Third edition) By Stephen G. Kochan - Page 41 - Question 8
